8eeeb2a059870b09860ca7db84b92ed6aa2446c9
loloof64 <laurent.bernabe@gmail.com>
2023-06-24 11:18:42 +0200

    slightly improved home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
-  <p>{{ $t('pages.home.description_1') }}</p>
-  <br />
-  <p>{{ $t('pages.home.description_2') }}<br />{{ $t('pages.home.description_3') }}</p>
+  <div class="content">
+    <p>{{ $t('pages.home.description_1') }}</p>
+    <br />
+    <p>{{ $t('pages.home.description_2') }}<br />{{ $t('pages.home.description_3') }}</p>
+  </div>
 </template>
 
 <script setup>
 import { useLocale } from 'vuetify'
 const t = useLocale();
 </script>
+
+<style scoped>
+.content {
+  padding: 20px;
+}
+</style>
